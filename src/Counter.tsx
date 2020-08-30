aad56877b32df023a95d30915a2f2db2fa1177c1
ayumubanban <ayumuban@outlook.com>
2020-08-30 19:40:14 +0900

    :sparkles: Study useRef

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,4 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
+// const array: Array<number> = [1, 2, 3];
+// const readOnlyArray: ReadonlyArray<number> = [1, 2, 3];
+// array[0] = 11;
+// readOnlyArray[0] = 11;
 
 // // type SetStateAction<S> = S | ((prevState: S) => S);
 // type MySetStateAction = number | ((prevState: number) => number);
@@ -21,11 +26,19 @@ const Counter: React.FC<{}> = () => {
     setValue((prevState) => prevState - 1);
   }
 
+  const renderTimes = useRef<number>(0);
+  // renderするたびに実行される（この場合は+-ボタン押した場合とかリロード時に）
+  useEffect(() => {
+    console.log('render was done.');
+    renderTimes.current = renderTimes.current + 1;
+  });
+
   return (
     <div>
       <div>value: {value}</div>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
+      <div>This component was re-rendered {renderTimes.current} times!</div>
     </div>
   );
 };
