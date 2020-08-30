9e4684afdc067036489aa63aa8a8d2463dcbc9f8
ayumubanban <ayumuban@outlook.com>
2020-08-30 19:07:16 +0900

    :sparkles: Study function components in typescript basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 
 interface AppProps {
-  message: string;
+  message?: string;
 }
 
 // propsの中身の型制約を宣言
 // const App = ({ message }: { message: string }) => {
-const App = ({ message }: AppProps) => {
+// const App = ({ message }: AppProps) => {
+// こっちの型制約の宣言の方が、defaultPropsに対する制限がきついので良いっぽい
+const App: React.FunctionComponent<AppProps> = ({ message }) => {
   // console.log(props);
   // const { message } = props;
   // return <div>React Starter Kit in TypeScript</div>;
   return <div>{message}</div>;
 };
+
+// デフォルトのpropsを設定できる
+App.defaultProps = {
+  message: 'Hello, defaultProps!',
+  // description: 'This is App Component.',
+}
 
 export default App;
